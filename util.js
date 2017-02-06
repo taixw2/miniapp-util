bada6e1cefcca6cb3fccb075576e3e3c2528e987
iqqq <254225961@qq.com>
2017-02-06 11:27:14 +0800

    =

--- util.js
+++ util.js
@@ -2,10 +2,32 @@ const promise = require('./promise.js').Promise;
 
 const noop = function() {};
 
+
+/**
+ * 转换称两位数
+ * 0 => 00
+ */
+function formatNumber(n) {
+    n = n.toString();
+    return n[1] ? n : '0' + n;
+}
+
+
+/**
+ * [秒 =》 分钟]
+ * 100 => 01:40
+ */
+function secondToMinute(s) {
+    let m = Math.floor(s / 60);
+    s = s % 60;
+    return [formatNumber(m), formatNumber(s)].join(":");
+}
+
+
 /**
  * 格式化时间
  */
-function formatTime(date, fmt = "yyyy/MM/dd hh:mm:ss") {
+exports.formatTime = function(date, fmt = "yyyy/MM/dd hh:mm:ss") {
     if (!date) return "";
     date = typeof date == "number" ? new Date(date) : date;
     var o = {
@@ -27,12 +49,12 @@ function formatTime(date, fmt = "yyyy/MM/dd hh:mm:ss") {
         }
     }
     return fmt;
-}
+};
 
 /**
  * 人性话格式时间
  */
-function ctDate(date) {
+exports.ctDate = function(date) {
     if (!date) return "";
     const now = Date.now();
     let diff;
@@ -55,30 +77,12 @@ function ctDate(date) {
             return formatTime(date);
     }
 
-}
-
-/**
- * [秒 =》 分钟]
- * 100 => 01:40
- */
-function secondToMinute(s) {
-    let m = Math.floor(s / 60);
-    s = s % 60;
-    return [formatNumber(m), formatNumber(s)].join(":");
-}
-
-/**
- * 转换称两位数
- */
-function formatNumber(n) {
-    n = n.toString();
-    return n[1] ? n : '0' + n;
-}
+};
 
 /**
  * 浅拷贝
  */
-function assign() {
+const assign = exports.assign = function() {
     const args = [].slice.apply(arguments);
     const target = args.shift();
     const length = args.length;
@@ -99,27 +103,117 @@ function assign() {
 
     return target;
 
-}
-
-
+};
 
 /**
  * 封装loadding
  */
-function loading(title = "加载中", duration = 10000, icon = "loading") {
+exports.loading = function(title = "加载中", duration = 10000, icon = "loading") {
     wx.showToast({
         title,
         icon,
         duration
     });
-}
+};
 
 /**
  * 封装hideLoading
  */
-function hideLoading() {
+exports.hideLoading = function() {
     wx.hideToast();
+};
+
+/**
+ * mode基类
+ */
+function modal(options, showCancel = false) {
+    return new promise((reslove, reject) => {
+
+        const SUCCESS = options.success || noop;
+        const FAIL = options.fail || noop;
+        options.success = function(res) {
+            SUCCESS(res);
+            if (res.confirm) {
+                reslove(true);
+            } else {
+                reslove(false);
+            }
+        };
+        options.fail = function(res) {
+            FAIL(res);
+            reject(res);
+        };
+        options.showCancel = showCancel;
+        wx.showModal(options);
+    });
 }
+
+/**
+ * 判断model的
+ * @param  {[type]} options [description]
+ * @return {[type]}         [description]
+ */
+function modalOptions (options) {
+  if (typeof options == "string") {
+      return {
+          title,
+          content: opt
+      };
+  }
+  return options;
+}
+
+/**
+ * 弹出层
+ */
+const _alert = exports._alert = function(options, title = "提示") {
+    options = modalOptions(options);
+    return model(options);
+};
+
+/**
+ * 对话框
+ */
+const _confirm = exports._confirm = function(options, title = "提示") {
+  options = modalOptions(options);
+  return modal(options,true);
+};
+
+
+exports.decodeHtml = function(domString) {
+    if (!domString) return "";
+
+    domString = typeof domString === "function" ? domString()  : domString.toString();
+
+    const REGX_HTML_DECODE = /&\w+;|&#(\d+);|<\w+>/g;
+    const HTML_DECODE = {
+        "&lt;": "<",
+        "&gt;": ">",
+        "&amp;": "&",
+        "&nbsp;": " ",
+        "&quot;": "\"",
+        "©": "",
+        "<br>": "\n",   //后端将\n转成了<br>
+    };
+
+    return domString.replace(REGX_HTML_DECODE,function(m,$1){
+      return HTML_DECODE[m] ? HTML_DECODE[m] : m;
+    });
+
+};
+
+//Object => queryString
+exports.param = function(obj) {
+    var key, val,
+        arr = [];
+    for (key in obj) {
+
+        val = obj[key];
+        arr[arr.length] = key + "=" + val;
+
+    }
+    return arr.join("&");
+};
 
 
 /**
@@ -130,31 +224,30 @@ function hideLoading() {
 function request(configuration) {
 
     const DEFALUT_CONFIG = {
-
-      root : "",
-      url : "",
-      method : "POST",
-      header : {
-        "content-type" : "application/x-www-form-urlencoded;charset=utf-8"
-      },
-      data : {},
-      success : noop,
-      fail : noop,
-      complete : noop,
-      resloveStatus :  noop,
-      loading : false,
+        root: "",
+        url: "",
+        method: "POST",
+        header: {
+            "content-type": "application/x-www-form-urlencoded;charset=utf-8"
+        },
+        data: {},
+        success: noop,
+        fail: noop,
+        complete: noop,
+        resloveStatus: noop,
+        loading: false,
     };
 
-    configuration = assign(DEFALUT_CONFIG,configuration);
+    configuration = assign({},DEFALUT_CONFIG, configuration);
 
     return function(requestParams) {
 
         return new promise(function(fulfill, reject) {
 
-                requestParams = assign(configuration,requestParams);
+                requestParams = assign({},configuration, requestParams);
 
                 if (requestParams.loading) {
-                  loading();
+                    loading();
                 }
 
                 wx.request({
@@ -165,7 +258,9 @@ function request(configuration) {
                     method: requestParams.method,
                     data: requestParams.data,
                     header: requestParams.header,
-                    success({ data }) {
+                    success({
+                        data
+                    }) {
 
                         /**
                          * 服务端通过code判断服务器超时/参数错误等
@@ -198,22 +293,45 @@ function request(configuration) {
             });
 
     };
-
 }
 
-
 /**
- * 获取自己的用户信息
- * @param  {Function} callback [获取信息之后的回调]
- * @param  {[type]}   force    [是否强制从服务器上拉去]
- * @return {[type]}            [promise]
+ * 配置获取用户信息的参数
+ * @return {[type]}               [getUserInfo]
  */
-function getUserInfo(callback, force) {
+function configUserInfo(userInfo) {
 
-    return new promise(function(reslove, reject) {
-        if (wx.code && !force) {
-            reslove(wx.code);
-        } else {
+    const DETAULT_USERINFO = {
+        code: "",
+        data: {},
+        /**
+         * 把认证信息传到服务器登录
+         * @param  {[type]} userInfo [认证信息]
+         * @param  {[type]} code     [登录code]
+         * @return {[type]}          [promise]
+         */
+        requestLogin: function(userInfo, code) {
+            //必须返回一个promise
+            return {
+                then: noop
+            };
+        }
+    };
+
+    userInfo = assign({},DETAULT_USERINFO, userInfo);
+
+    /**
+     * 获取自己的用户信息
+     * @param  {Function} callback [获取信息之后的回调]
+     * @param  {[type]}   force    [是否强制从服务器上使用新的code拉取用户信息]
+     * @return {[type]}            [promise]
+     */
+    return function getUserInfo(callback = noop, force = false) {
+        return new promise(function(reslove, reject) {
+            if (userInfo.code && !force) {
+                reslove(userInfo.code);
+                return;
+            }
             wx.login({
                 success(res) {
                     wx.code = res.code;
@@ -224,54 +342,36 @@ function getUserInfo(callback, force) {
                     reject(res);
                 }
             });
-        }
-    }).then(function(code) {
-        return !force && wx.userInfo ||
-            new promise(function(reslove, reject) {
+        }).then(function(code) {
+            if (!force && userInfo.data) return userInfo.data;
+            return new promise(function(reslove, reject) {
                 wx.getUserInfo({
                     success(res) {
-                        reslove({
-                            code,
-                            res
-                        });
+                        reslove(res);
                     },
                     fail(res) {
-                        console.log(res);
-                        reject({
-                            code,
-                            res
-                        });
+                        reject(res);
                     }
                 });
             });
-
-    }).then(
-        function(res) {
-            return !force && wx.userInfo ||
-                request({
-                    url: "user/cgi_user_login",
-                    data: {
-                        source: 1,
-                        code: res.code,
-                        raw_data: res.res.rawData,
-                        signature: res.res.signature,
-                        encrypted_data: res.res.encryptedData,
-                        iv: res.res.iv
-                    }
-                });
-        },
-        function(res) {
-            console.log(res);
-            _alert("登录失败，请重新登录")
-                .then(res => {
-                    getUserInfo(callback, true);
-                });
-            throw new Error("login fail");
-        }
-    ).then(function(res) {
-        wx.userInfo = res;
-        return callback && callback(res) || res;
-    });
+        }).then(
+            function(res) {
+                if (!force && userInfo.data) return userInfo.data;
+                return userInfo.requestLogin(res, userInfo.code);
+            },
+            function(res) {
+                console.log(res);
+                _alert("登录失败，请重新登录")
+                    .then(res => {
+                        getUserInfo(callback, true);
+                    });
+                throw new Error("login fail:" + res);
+            }
+        ).then(function(res) {
+            userInfo.data = res;
+            return callback(res) || res;
+        });
+    };
 }
 
 /**
@@ -294,154 +394,65 @@ function payment(payParams) {
     });
 }
 
-
-
 /**
  * 上传文件
  * @param  {[type]} opt [description]
  * @return {[type]}     [description]
  */
-function uploadFile(opt) {
-    return new promise(function(reslove, reject) {
-        const base_path = "https://www.huxiao.com/cgi-bin/pa/q/";
-        opt.success = function(res) {
-            let data;
+function uploadFile(uploadFileConfig) {
 
-            try {
-                data = JSON.parse(res.data);
-            } catch (e) {
-                data = {};
-            }
+    const DEFAULT_UPLOAD_CONFIG = {
 
-            console.log(data);
-            if (data.ec !== 0) {
-                reject(res);
-            } else {
-                reslove(data);
-            }
-
-        };
-        opt.fail = function(res) {
-            console.log(res);
-            reject(res);
-        };
-        // opt.complete = function() {
-        //   hideLoading();
-        // };
-        opt.url = opt.url && (base_path + opt.url) || base_path + "common/cgi_common_pic_upload";
-        console.log(opt);
-        wx.uploadFile(opt);
-    });
-}
-
-function uploadFiles(opts) {
-    var paths = opts.filePaths;
-    opts.filePaths = null;
-    return promise.all(paths.map(v => {
-        opts.filePath = v;
-        return uploadFile(opts);
-    }));
-}
-
-function modal(opt) {
-    return new promise((reslove, reject) => {
-        var success = opt.success || function() {};
-        var fail = opt.fail || function() {};
-        opt.success = function(res) {
-            success();
-            if (res.confirm) {
-                reslove(true);
-            } else {
-                reslove(false);
-            }
-        };
-        opt.fail = function() {
-            fail();
-            reject();
-        };
-        wx.showModal(opt);
-    });
-
-}
-
-function _alert(opt, title = "提示") {
-    var content;
-    if (typeof opt == "string") {
-        opt = {
-            title,
-            content: opt
-        };
-    }
-    opt.showCancel = false;
-    return modal(opt);
-}
-
-function _confirm(opt, title = "提示") {
-
-    var content;
-    if (typeof opt == "string") {
-        opt = {
-            title,
-            content: opt
-        };
-    }
-
-    return modal(opt);
-}
-
-function decodeHtml(s) {
-    var HTML_DECODE = {
-        "&lt;": "<",
-        "&gt;": ">",
-        "&amp;": "&",
-        "&nbsp;": " ",
-        "&quot;": "\"",
-        "©": "",
-        "<br>": "\n",
-    };
-    var REGX_HTML_DECODE = /&\w+;|&#(\d+);|<\w+>/g;
-
-    s = (s !== undefined) ? s : "";
-    return (typeof s != "string") ? s : s.replace(REGX_HTML_DECODE, function($0, $1) {
-        var c = HTML_DECODE[$0];
-        if (c === undefined) {
-            if (!isNaN($1)) {
-                c = String.fromCharCode(($1 == 160) ? 32 : $1);
-            } else {
-                c = $0;
-            }
+        root: "",
+        defaultURL: "",
+        resloveStatus: noop,
+        requestParams: {
+            header: {},
+            name: ""
         }
-        return c;
-    });
+
+    };
+
+    uploadFileConfig = assign({}, DEFAULT_UPLOAD_CONFIG, uploadFileConfig);
+
+    return function(options) {
+
+        return new promise(function(fulfill, reject) {
+
+            options = assign({}, uploadFileConfig.requestParams, options);
+            options.url = options.url ? uploadFileConfig.root + options.url : uploadFileConfig.root + uploadFileConfig.defaultURL;
+
+            options.success = function(res) {
+                let fileData;
+                try {
+                    fileData = JSON.parse(res.data);
+                } catch (e) {
+                    reject(e);
+                }
+                if (uploadFileConfig.resloveStatus(fileData)) {
+                    fulfill(fileData);
+                } else {
+                    reject(fileData);
+                }
+            };
+
+            options.fail = function(res) {
+                console.log(res);
+                reject(res);
+            };
+
+            wx.uploadFile(options);
+        });
+
+    };
 }
 
-function param(obj) {
-    var key, val,
-        arr = [];
-    for (key in obj) {
+exports.pubsub = {
 
-        val = obj[key];
-        arr[arr.length] = key + "=" + val;
+  cache : {},
 
-    }
-    return arr.join("&");
-}
+  $on () {},
+  $emit () {}
 
 
-module.exports = {
-    formatTime,
-    getUserInfo,
-    request,
-    uploadFiles,
-    uploadFile,
-    param,
-    loading,
-    decodeHtml,
-    hideLoading,
-    payment,
-    alert: _alert,
-    ctDate: ctDate,
-    confirm: _confirm,
-    getOtherInfo,
-    secondToMinute
 };
